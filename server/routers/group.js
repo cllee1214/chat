34c1f34f1fe45f72812d0e10e9ec7bc65018355b
yunhong.cai <yunhong.cai@1905.com>
2020-08-01 23:20:28 +0800

    修复创建群逻辑

--- server/routers/group.js
+++ server/routers/group.js
@@ -1,22 +1,31 @@
 var express = require('express');
 var router = express.Router();
 var Group = require('../db/models.js').group
+var User = require('../db/models.js').user
 
 router.post('/create', function(req, res) {
     var name = req.body.name
     var needValidate = req.body.validate
     var user = req.body.user
+    var gid = parseInt(Math.random() * 1000000) + ''
     var group = new Group({
         name,
         needValidate,
         creatBy: user,
         users: [user],
-        id: parseInt(Math.random() * 1000000) + ''
+        id: gid
     })
     group.save().then(data => {
-        res.json({
-            code: '1',
-            msg: '群创建成功'
+        User.where({user}).updateOne({$push: {groups: gid}}).then(result => {
+            res.json({
+                code: '1',
+                msg: '群创建成功'
+            })
+        }).catch(err => {
+            res.json({
+                code: '0',
+                msg: '创建群失败'
+            })
         })
     }).catch(err => {
         res.json({
